fbc567c1f8f34806d6db41171ef75fddb90309ae
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-19 18:50:03 -0600

    If-then-script.sh

---
+++ If-then-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Date: Feb 19 2024
+# Author: Wilfredo S
+# This Script is an example of if-then statments 
+
+#Variables
+count=100
+
+if [ "${count}" -eq 100 ] 
+then
+  echo "Count is 100" 
+else
+  echo "Count is not 100"
+fi
