9c26873384535eb58a278b3a284a9931f9530415
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-19 19:03:47 -0600

    Create case-statements.sh

---
+++ case-statements.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Date: Mon 19 Feb 2024
+# Author: Wilfredo S
+# This Script is an example of Case statements scripts
+
+echo
+echo "Please chose one of the options below" 
+echo
+echo "a = Display Date and Time"
+echo "b = List of file and directories" 
+echo "c = list users logged in"
+echo "d = Check system uptime" 
+echo
+
+  read choices 
+
+  Case $choices in
+
+a) date;;
+b) ls;;
+c) who;;
+d) uptime;; 
+*) echo "Invalid choice -bye."
+exit 1
