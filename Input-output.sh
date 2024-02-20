90b5c6832c97919fe77690271cd61f64b48f3200
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-19 18:33:52 -0600

    Create Input-output.sh

---
+++ Input-output.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Date: Mon 19 Feb 2024
+# Author: Wilfredo S
+# This script will make you add information to the script 
+
+#Variables
+a=hostname
+b=name
+
+echo "Hello my server name is ${a}"
+echo
+echo "What is your name ${b}"
+read ${b}
+echo
+echo "Hello ${b}
