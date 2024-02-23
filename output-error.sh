9e65aae891f7061eda7adefc4ea76b2daf09623f
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-22 21:34:30 -0600

    Create output-error.sh

---
+++ output-error.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Date Feb 22 2024
+# Author: Wilfredo S
+# This script will pull errors from a file
+
+grep -i error /home/wsantamaria/professional-scripts/messages > /home/wsantamaria/professional-scripts/output-error
