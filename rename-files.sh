4315f62b09203fa0f388463a7075220778c59ad6
was9419 <47761851+was9419@users.noreply.github.com>
2024-02-26 19:30:25 -0600

    Create rename-files.sh

---
+++ rename-files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# Author: Wilfredo S
+# Date: Mon 26 Feb 2024
+# Description: Example of deleting a file with a scripts 
+# This scrip will rename files older than 90 days
+
+find /home/wsantamaria/professional-scripts/old-files -mtime +90 -exec mv {} {}.old \;
