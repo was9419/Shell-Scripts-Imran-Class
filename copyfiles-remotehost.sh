92f0d1bd75806eaeb786c4e1865d1b7490d460d8
was9419 <47761851+was9419@users.noreply.github.com>
2024-03-04 21:14:45 -0600

    Create copyfiles-remotehost.sh

---
+++ copyfiles-remotehost.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author: Wilfredo S
+# Date: Mon 24 March 2024
+# This Script will serve as an example of creating multiple files with different names
+
+for i in {1..10}
+do 
+  touch imran.$1
+done
