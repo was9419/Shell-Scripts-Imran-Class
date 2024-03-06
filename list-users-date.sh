a9709f7fc8970a501c0cbac07ccff22ce6e50307
was9419 <47761851+was9419@users.noreply.github.com>
2024-03-05 19:51:58 -0600

    Create list-users-date.sh

---
+++ list-users-date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Wilfredo S
+# Date : March 5 2024
+# Description: This script will server as example for listing users by date
+
+# Variables
+TODAY="date | awk 'print $1,$2,$3'"
+#####
+
+last | grep "$TODAY"
+
+
